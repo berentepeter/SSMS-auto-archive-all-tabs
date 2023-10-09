04afc9ddddee0fffe0761d9327b89562e183963a
Berente Péter <40863181+berentepeter@users.noreply.github.com>
2023-10-09 15:05:26 +0200

    Add files via upload

---
+++ SQL_Backup_autosave.ps1
@@ -0,0 +1,8 @@
+﻿## SSMS ##
+$path = 'C:\Users\berentep\Documents\Visual Studio 2017\Backup Files\Solution1'
+$name = Get-Date -UFormat "%Y/%m/%d"
+if(!(Test-Path -Path C:\Folder)){New-Item -ItemType Directory -Force -Path C:\Folder}
+New-Item -ItemType Directory -Force -Path C:\Folder\$name
+Copy-Item -Path $path -Destination C:\Folder\$name -Recurse
+
+exit 10
